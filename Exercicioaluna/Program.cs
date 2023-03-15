47628802127b7c57f24b614ec01da1f7926f180e
Nicole Caroline <100379639+projetosnicole@users.noreply.github.com>
2023-03-15 15:34:30 -0300

    Add files via upload

---
+++ Exercicioaluna/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicioaluna;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "FULANO DA SILVA,";
+        aluno1.nota1 = 8;
+        aluno1.nota2 = 5;
+
+        aluno1.mensagem();
+    }
+}
