3383b46c5174bf3d8e09a35bd9203f42f7838b76
Nicole Caroline <100379639+projetosnicole@users.noreply.github.com>
2023-03-15 15:44:52 -0300

    Add files via upload

---
+++ exemplo/Aluno.cs
@@ -0,0 +1,28 @@
+namespace exemplo;
+class Aluno
+{
+    public string nome="";
+    public double nota1, nota2;
+
+     public double obterMedia(){
+        double media = (nota1+nota2)/2;
+        return media;
+    }
+
+    public string ObterSituacao(double media){
+        string situacao = "";
+        if (media>=6){
+            situacao= " Aprovado ";
+        }
+        else{
+            situacao= " Reprovado ";
+        }
+        return situacao;
+    }
+
+    public void mensagem(){
+        double mediaCalculada = obterMedia();
+        string resultadoSituacao = ObterSituacao(mediaCalculada);
+        Console.WriteLine( " O aluno " + nome + " obteve média= " + mediaCalculada + " portanto, está" + resultadoSituacao);
+    }
+}
---
+++ exemplo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace exemplo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+
+        Console.WriteLine("Digite seu nome: ");
+        string? nome = Console.ReadLine();
+        //Console.WriteLine(" Seu nome é: " + nome);
+
+        Console.WriteLine("Digite sua primeira nota: ");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("O número digitado foi: " + nota1);
+
+        Console.WriteLine("Digite sua segunda nota: ");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("O número digitado foi: " + nota2);
+
+        aluno1.nome = nome="";
+        aluno1.nota1 = nota1;
+        aluno1.nota2 = nota2;
+
+        aluno1.mensagem();
+
+
+    }
+}
